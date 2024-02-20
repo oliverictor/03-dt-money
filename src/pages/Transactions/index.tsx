65c973ef579737e646f0d6b18d79b1782c679c0a
victor.oliveira <victor.oliveira@hexagon.pro>
2024-02-20 17:22:13 -0300

    add

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
@@ -9,27 +10,28 @@ export function Transactions() {
             <Summary />
 
             <TransactionsContainer>
+                <SearchForm />
                 <TransactionsTable>
                     <body>
                         <tr>
                             <td width="50%">Desenvolvimento de site</td>
-                            <td>
+                            <td width="20%">
                                 <PriceHighLight variant="income">
                                     R$ 12.000,00
                                 </PriceHighLight>
                             </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
+                            <td width="15%">Venda</td>
+                            <td width="15%">13/04/2022</td>
                         </tr>
                         <tr>
                             <td width="50%">Hamburguer</td>
-                            <td>
+                            <td width="20%">
                                 <PriceHighLight variant="outcome">
                                     - R$ 59,00
                                 </PriceHighLight>
                             </td>
-                            <td>Alimentação</td>
-                            <td>10/04/2022</td>
+                            <td width="15%">Alimentação</td>
+                            <td width="15%">10/04/2022</td>
                         </tr>
                     </body>
                 </TransactionsTable>
