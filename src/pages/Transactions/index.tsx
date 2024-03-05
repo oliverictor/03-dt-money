aa7ab5d1931cb0477db1fcc23c8300757ee489ef
victor.oliveira <victor.oliveira@hexagon.pro>
2024-03-05 00:16:16 -0300

    Formata valores e data

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,10 +4,11 @@ import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
     const { transactions } = useContext(TransactionsContext)
-    
+
     return (
         <div>
             <Header />
@@ -22,11 +23,12 @@ export function Transactions() {
                                     <td width="50%">{transaction.description}</td>
                                     <td width="20%">
                                         <PriceHighLight variant={transaction.type}>
-                                            {transaction.price}
+                                            {transaction.type === 'outcome' && '- '}
+                                            {priceFormatter.format(transaction.price)}
                                         </PriceHighLight>
                                     </td>
                                     <td width="15%">{transaction.category}</td>
-                                    <td width="15%">{transaction.createdAt}</td>
+                                    <td width="15%">{dateFormatter.format(new Date(transaction.createdAt))}</td>
                                 </tr>
                             )
                         })}
