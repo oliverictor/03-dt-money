392524fda08841a445db8501c0dff5ff8e38afbf
victor.oliveira <victor.oliveira@hexagon.pro>
2024-03-26 22:42:47 -0300

    add eslint

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,76 +1,76 @@
-import { ReactNode, createContext, useEffect, useState } from "react"
-import { api } from "../lib/axios"
+import { ReactNode, createContext, useEffect, useState } from 'react'
+import { api } from '../lib/axios'
 
 interface Transaction {
-    id: number
-    description: string
-    type: 'income' | 'outcome'
-    category: string
-    price: number
-    createdAt: string
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  category: string
+  price: number
+  createdAt: string
 }
 
 interface CreateTransactionInput {
-    description: string
-    price: number
-    category: string
-    type: 'income' | 'outcome'
+  description: string
+  price: number
+  category: string
+  type: 'income' | 'outcome'
 }
 
 interface TransactionContextType {
-    transactions: Transaction[]
-    fetchTransactions: (query?: string) => Promise<void>
-    createTransaction: (data: CreateTransactionInput) => Promise<void>
+  transactions: Transaction[]
+  fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (data: CreateTransactionInput) => Promise<void>
 }
 
 export const TransactionsContext = createContext({} as TransactionContextType)
 
 interface TransactionProviderProps {
-    children: ReactNode
+  children: ReactNode
 }
 
 export function TransactionProvider({ children }: TransactionProviderProps) {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
+  const [transactions, setTransactions] = useState<Transaction[]>([])
 
-    async function fetchTransactions(query?: string) {
-        const response = await api.get('transactions', {
-            params: {
-                _sort: 'createdAt',
-                _order: 'desc',
-                q: query
-            }
-        })
+  async function fetchTransactions(query?: string) {
+    const response = await api.get('transactions', {
+      params: {
+        _sort: 'createdAt',
+        _order: 'desc',
+        q: query,
+      },
+    })
 
-        setTransactions(response.data);
-    }
+    setTransactions(response.data)
+  }
 
-    async function createTransaction(data: CreateTransactionInput) {
-        const { description, category, price, type } = data
+  async function createTransaction(data: CreateTransactionInput) {
+    const { description, category, price, type } = data
 
-        const response = await api.post('transactions', {
-            description,
-            category,
-            price,
-            type,
-            createdAt: new Date()
-        })
+    const response = await api.post('transactions', {
+      description,
+      category,
+      price,
+      type,
+      createdAt: new Date(),
+    })
 
-        setTransactions(state => [response.data, ...state]);
-    }
+    setTransactions((state) => [response.data, ...state])
+  }
 
-    useEffect(() => {
-        fetchTransactions()
-    }, [])
+  useEffect(() => {
+    fetchTransactions()
+  }, [])
 
-    return (
-        <TransactionsContext.Provider
-            value={{
-                transactions,
-                fetchTransactions,
-                createTransaction
-            }}
-        >
-            {children}
-        </TransactionsContext.Provider>
-    )
+  return (
+    <TransactionsContext.Provider
+      value={{
+        transactions,
+        fetchTransactions,
+        createTransaction,
+      }}
+    >
+      {children}
+    </TransactionsContext.Provider>
+  )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,40 +1,46 @@
-import { useContext } from "react";
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { SearchForm } from "./components/SearchForm";
-import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
-import { dateFormatter, priceFormatter } from "../../utils/formatter";
+import { useContext } from 'react'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 
 export function Transactions() {
-    const { transactions } = useContext(TransactionsContext)
+  const { transactions } = useContext(TransactionsContext)
 
-    return (
-        <div>
-            <Header />
-            <Summary />
-            <TransactionsContainer>
-                <SearchForm />
-                <TransactionsTable>
-                    <body>
-                        {transactions.map(transaction => {
-                            return (
-                                <tr key={transaction.id}>
-                                    <td width="50%">{transaction.description}</td>
-                                    <td width="20%">
-                                        <PriceHighLight variant={transaction.type}>
-                                            {transaction.type === 'outcome' && '- '}
-                                            {priceFormatter.format(transaction.price)}
-                                        </PriceHighLight>
-                                    </td>
-                                    <td width="15%">{transaction.category}</td>
-                                    <td width="15%">{dateFormatter.format(new Date(transaction.createdAt))}</td>
-                                </tr>
-                            )
-                        })}
-                    </body>
-                </TransactionsTable>
-            </TransactionsContainer>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Summary />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <body>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td width="20%">
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td width="15%">{transaction.category}</td>
+                  <td width="15%">
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </body>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
 }
