1a4e6341aca9307f204d4b9ec8ce3d6e794446f9
victor.oliveira <victor.oliveira@hexagon.pro>
2024-03-28 22:20:18 -0300

    aplica useCallback

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,10 +1,10 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
-import { SearchFormContainer } from './styles'
+import { useContextSelector } from 'use-context-selector'
 import * as z from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { SearchFormContainer } from './styles'
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -13,7 +13,13 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
+
   const {
     register,
     handleSubmit,
