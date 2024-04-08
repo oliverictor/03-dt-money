1c8d9b3c7bacf5b979aec94b92772dd8034ec8fb
victor.oliveira <victor.oliveira@hexagon.pro>
2024-04-08 08:46:28 -0300

    adiciona comentário memo

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -6,6 +6,23 @@ import * as z from 'zod'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 import { SearchFormContainer } from './styles'
 
+/*
+ * Por que que um componente rendeiza?
+ * - Hooks changed (mudou estado, contexto, reducer);
+ * - Props changed (mudou propriedade);
+ * - Parent rerendered (componente pai renderizou);
+ *
+ * Qual o fluxo de renderização?
+ * 1. O React recria o HTML da interface daquele componente
+ * 2. Compara a versão do HTML recriada com a versão anterior
+ * 3. SE mudou alguma coisa, ele reescreve o HTML na tela
+ *
+ * Memo:
+ * 0.Hooks changed, Props changed (deep comparison)
+ * 0.1 Comparar a versão anterior dos hooks e props
+ * 0.2 SE mudou algo, ele vai permitir a nova renderização
+ */
+
 const searchFormSchema = z.object({
   query: z.string(),
 })
